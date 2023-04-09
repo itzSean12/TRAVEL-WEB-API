b75eb3e2c21141f8e4d862f98bf9bb5ff7c319ec
itzSean12 <ordonezsean@yahoo.com>
2023-04-09 00:05:16 -0400

    updating mongodb

--- db.js
+++ db.js
@@ -3,10 +3,13 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://jaredgomez0812:Emanuel12@cluster0.o7elciy.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://jaredgomez0812:Emanuel12@cluster0.o7elciy.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
     );
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
